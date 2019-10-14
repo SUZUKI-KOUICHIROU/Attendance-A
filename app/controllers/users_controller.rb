afe475204da349d527b33f43f87298b85f3b06ec
KOUICHIROUSUZUKI <j5303316@yahoo.co.jp>
2019-10-14 20:01:23 +0000

    ページネーション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
   end
@@ -55,14 +58,9 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
-
+    
     # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
-
-
-
-

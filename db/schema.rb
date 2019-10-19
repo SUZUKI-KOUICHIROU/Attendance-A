7b88a5fa40e3016af04810603dbe6113fb5dfcdb
KOUICHIROUSUZUKI <j5303316@yahoo.co.jp>
2019-10-19 21:43:36 +0000

    10.6.2updateアクション完成

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20191016191711) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2019-10-15 23:00:00"
-    t.datetime "work_time", default: "2019-10-15 22:30:00"
+    t.datetime "basic_time", default: "2019-10-19 23:00:00"
+    t.datetime "work_time", default: "2019-10-19 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

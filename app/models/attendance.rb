cb7d1f5b1caa2393e99b31ecf2fd3069b092f64f
KOUICHIROUSUZUKI <j5303316@yahoo.co.jp>
2019-11-10 18:36:34 +0000

    管理者翌日以降制限

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,13 +3,10 @@ class Attendance < ApplicationRecord
 
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
-
-
-
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
   # 退勤時間が存在しない場合、出勤時間は無効
-  validate :started_at_is_invalid_without_a_started_at
+  validate :started_at_is_invalid_without_a_finished_at
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
 
@@ -18,9 +15,9 @@ class Attendance < ApplicationRecord
   end
 
   def started_at_is_invalid_without_a_finished_at
-    errors.add(:finished_at, "が必要です") if started_at.present? && finished_at.blank?
-  end
-
+    errors.add(:finished_at, "が必要です") if finished_at.blank? && started_at.present?
+  end 
+   
   def started_at_than_finished_at_fast_if_invalid
     if started_at.present? && finished_at.present?
       errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
